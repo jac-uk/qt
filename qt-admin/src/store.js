66f57d9441e6b76d19e166be6eaad4cc1889dd54
Warren Searle <warren@precise-minds.co.uk>
2022-05-15 10:22:07 +0100

    Update store

--- qt-admin/src/store.js
+++ qt-admin/src/store.js
@@ -7,33 +7,11 @@ firestoreOptions.wait = true;
 // Vuex modules
 import auth from '@/store/auth';
 import services from '@/store/services';
-import exerciseCollection from '@/store/exercise/collection';
-import exerciseCreateJourney from '@/store/exercise/createJourney';
-import exerciseDocument from '@/store/exercise/document';
-import applications from '@/store/applications';
-import application from '@/store/application';
-import assessment from '@/store/assessment';
-import assessments from '@/store/assessments';
-import notifications from '@/store/notifications';
-import invitations from '@/store/invitations';
-import events from '@/store/events';
 import clipboard from '@/store/clipboard';
 
-import stageReview from '@/store/stage/review';
-import stageSelected from '@/store/stage/selected';
-import stageHandover from '@/store/stage/handover';
-import stageRecommended from '@/store/stage/recommended';
-import stageShortlisted from '@/store/stage/shortlisted';
-import characterChecks from '@/store/characterChecks/characterChecks';
-import candidates from '@/store/candidates';
-import notes from '@/store/notes';
-import candidateApplications from '@/store/candidateApplications';
 import qualifyingTest from '@/store/qualifyingTest/qualifyingTest';
 import qualifyingTestResponses from '@/store/qualifyingTest/qualifyingTestResponses';
 import connectionMonitor from '@/store/connectionMonitor';
-import qualifyingTestReports from '@/store/qualifyingTestReports/collection';
-import qualifyingTestReport from '@/store/qualifyingTestReports/document';
-import panels from '@/store/panels';
 
 const store = new Vuex.Store({
   // Don't use strict mode in production for performance reasons (https://vuex.vuejs.org/guide/strict.html)
@@ -41,32 +19,10 @@ const store = new Vuex.Store({
   modules: {
     auth,
     services,
-    exerciseCollection,
-    exerciseCreateJourney,
-    exerciseDocument,
-    applications,
-    application,
-    assessment,
-    assessments,
-    events,
     clipboard,
-    notifications,
-    invitations,
-    stageReview,
-    stageSelected,
-    stageHandover,
-    stageRecommended,
-    stageShortlisted,
-    candidates,
-    notes,
-    candidateApplications,
     qualifyingTest,
     qualifyingTestResponses,
     connectionMonitor,
-    qualifyingTestReports,
-    qualifyingTestReport,
-    panels,
-    characterChecks,
   },
   state: {
     packageVersion: process.env.PACKAGE_VERSION || '0',
