4e1327628e08f20ec1a76ee8626f654af2ac2626
Warren Searle <warren@precise-minds.co.uk>
2022-05-17 17:43:09 +0100

    Firebase. Remove unused files

--- firebase/functions/backgroundFunctions/onExerciseCreate.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-
-module.exports = functions.region('europe-west2').firestore
-  .document('exercises/{exerciseId}')
-  .onCreate((snap, context) => {
-    const exerciseId = context.params.exerciseId;
-    console.log(`Exercise created (${exerciseId})`);
-  });
